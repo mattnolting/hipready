e91f352125d5c5a2beb28abf3903cbf80488163e
mnolting <matthew.nolting@gmail.com>
2014-10-20 19:34:31 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<!--<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1">-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script type="text/javascript" src="assets/js/vendors/modernizr.custom.js"></script>
+	<link rel="stylesheet" href="assets/css/style.min.css">
+
+
+</head>
+
+<body>
+
+	<header class="banner masthead" role="banner">
+		<div class="container">
+			<div class="row">
+				<nav role="navigation">
+
+				</nav>
+			</div>
+		</div>
+	</header>
+
+
+	<main class="wrap container page" role="main">
+
+	</main>
+
+	<footer class="content-info" role="contentinfo">
+
+	</footer>
+
+	<script type="text/javascript">
+	</script>
+
+	<script type="text/javascript" src="assets/js/script.min.js"></script>
+
+</body>
+</html>
